6ff626aab5526ec4624661de27084dbeb16a9dac
Ge0rges <ge0rges@ge0rges.com>
2017-02-11 18:13:19 +0200

    Initial Commit

---
+++ Synaction/Synaction.h
@@ -0,0 +1,13 @@
+//
+//  Synaction.h
+//  Synaction
+//
+//  Created by Georges Kanaan on 11/02/2017.
+//  Copyright © 2017 Georges Kanaan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Synaction : NSObject
+
+@end
---
+++ Synaction/Synaction.m
@@ -0,0 +1,13 @@
+//
+//  Synaction.m
+//  Synaction
+//
+//  Created by Georges Kanaan on 11/02/2017.
+//  Copyright © 2017 Georges Kanaan. All rights reserved.
+//
+
+#import "Synaction.h"
+
+@implementation Synaction
+
+@end
