8f7a8053ce2b8b113a6ea2e68e7e0692cbeae115
Ge0rges <ge0rges@ge0rges.com>
2019-10-16 18:52:57 -0400

    xcode

--- build/Synaction.build/Debug-iphoneos/Synaction.build/Objects-normal/arm64/ConnectivityManager.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/GeorgesKanaan/Documents/Development/iOS_Development/Synaction/Synaction/ConnectivityManager.m \
-  /Users/GeorgesKanaan/Documents/Development/iOS_Development/Synaction/Synaction/Synaction.h
--- build/Synaction.build/Debug-iphoneos/Synaction.build/Objects-normal/arm64/Synaction.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/GeorgesKanaan/Documents/Development/iOS_Development/Synaction/Synaction/Synaction.m \
-  /Users/GeorgesKanaan/Documents/Development/iOS_Development/Synaction/Synaction/Synaction.h
